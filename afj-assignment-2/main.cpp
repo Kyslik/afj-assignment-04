6a47f1363bdf13522c2fc6e89b8617150aaac31e
Martin Kiesel <martin.kiesel@gmail.com>
2016-03-11 15:55:34 +0100

    Initial Commit

---
+++ afj-assignment-2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  afj-assignment-2
+//
+//  Created by Martin Kiesel on 11/03/16.
+//  Copyright © 2016 Martin Kiesel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
