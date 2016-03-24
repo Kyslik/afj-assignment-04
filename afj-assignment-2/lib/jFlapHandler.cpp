401ef47b58a7608ec9ecf3f6264ca91bfc826236
Martin Kiesel <martin.kiesel@gmail.com>
2016-03-24 16:17:46 +0100

    project structure && settings, new data files and classes

---
+++ afj-assignment-2/lib/jFlapHandler.cpp
@@ -0,0 +1,9 @@
+//
+//  jFlapHandler.cpp
+//  afj-assignment-2
+//
+//  Created by Martin Kiesel on 24/03/16.
+//  Copyright © 2016 Martin Kiesel. All rights reserved.
+//
+
+#include "jFlapHandler.hpp"
